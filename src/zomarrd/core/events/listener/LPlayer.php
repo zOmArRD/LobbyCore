5bf43f7ce660956a908ccef731a3c83a8a6788ed
zOmArRD <omarsena25@gmail.com>
2021-09-26 19:58:47 -0400

    LPlayer.php (Added proxy events)

---
+++ src/zomarrd/core/events/listener/LPlayer.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Created by PhpStorm.
+ *
+ * User: zOmArRD
+ * Date: 26/9/2021
+ *
+ * Copyright © 2021 Greek Network - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace zomarrd\core\events\listener;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use ReflectionClass;
+use ReflectionException;
+
+final class LPlayer implements Listener
+{
+    /** @var array  */
+    private array $login, $join, $move;
+
+    /**
+     * @throws ReflectionException
+     */
+    public function onDataReceive(DataPacketReceiveEvent $ev): void
+    {
+        $pk = $ev->getPacket();
+        $pl = $ev->getPlayer();
+
+        switch (true) {
+            case $pk instanceof LoginPacket:
+                $class = new ReflectionClass($pl);
+
+                /* Check if the player has the true ip and not the proxy.  */
+                if (isset($pk->clientData["Waterdog_IP"])) {
+                    $property = $class->getProperty("ip");
+                    $property->setAccessible(true);
+                    $property->setValue($pl, $pk->clientData["Waterdog_IP"]);
+                }
+
+                /* Check the XUID of the proxied player. */
+                if (isset($pk->clientData["Waterdog_XUID"])) {
+                    $property = $class->getProperty("xuid");
+                    $property->setAccessible(true);
+                    $property->setValue($pl, $pk->clientData["Waterdog_XUID"]);
+                    $pk->xuid = $pk->clientData["Waterdog_XUID"];
+                }
+                break;
+        }
+    }
+}
