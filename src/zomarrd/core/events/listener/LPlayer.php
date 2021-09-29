0a0b4c14bdb098fd4e948617f10fddc7691dc1cd
zOmArRD <omarsena25@gmail.com>
2021-09-28 20:32:03 -0400

    LPlayer.php (Added important events)

--- src/zomarrd/core/events/listener/LPlayer.php
+++ src/zomarrd/core/events/listener/LPlayer.php
@@ -11,18 +11,43 @@ declare(strict_types=1);
 
 namespace zomarrd\core\events\listener;
 
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCreationEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\level\Position;
 use pocketmine\network\mcpe\protocol\EmotePacket;
 use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\utils\TextFormat;
 use ReflectionClass;
 use ReflectionException;
+use zomarrd\core\modules\mysql\AsyncQueue;
+use zomarrd\core\modules\mysql\query\InsertQuery;
+use zomarrd\core\modules\mysql\query\SelectQuery;
 use zomarrd\core\network\Network;
+use zomarrd\core\network\player\NetworkPlayer;
+use zomarrd\core\network\session\Session;
+use zomarrd\core\network\utils\TextUtils;
+use const zOmArRD\Spawn_Data;
 
 final class LPlayer implements Listener
 {
-    /** @var array  */
+    /** @var array */
     private array $login, $join, $move;
+
+    public function getNetwork(): Network
+    {
+        return new Network();
+    }
 
     /**
      * @throws ReflectionException
@@ -55,6 +80,161 @@ final class LPlayer implements Listener
                 $emoteId = $pk->getEmoteId();
                 (new Network())->getServerPM()->broadcastPacket($pl->getViewers(), EmotePacket::create($pl->getId(), $emoteId, 1 << 0));
                 break;
+        }
+    }
+
+    public function onPlayerCreation(PlayerCreationEvent $ev): void
+    {
+        $ev->setPlayerClass(NetworkPlayer::class);
+    }
+
+    /**
+     * @todo finalize someone functions.
+     *
+     * @param PlayerPreLoginEvent $ev
+     */
+    public function onPreLogin(PlayerPreLoginEvent $ev): void
+    {
+        $player = $ev->getPlayer();
+
+        if (!$player instanceof NetworkPlayer) return;
+
+        $pn = $player->getName();
+        $player->setLangSession();
+
+        AsyncQueue::submitQuery(new SelectQuery("SELECT * FROM settings WHERE player='$pn';"), function ($result) use ($pn) {
+            $lang = "eng";
+            if (sizeof($result) === 0) {
+                AsyncQueue::submitQuery(new InsertQuery("INSERT INTO settings(player, language, scoreboard) VALUES ('$pn', '$lang', 1);"));
+            }
+        });
+    }
+
+    public function onLoging(PlayerLoginEvent $ev): void
+    {
+        $player = $ev->getPlayer();
+
+        if (!$player instanceof NetworkPlayer) return;
+
+        $pn = $player->getName();
+
+        AsyncQueue::submitQuery(new SelectQuery("SELECT * FROM settings WHERE player='$pn';"), function ($result) use ($player, $pn){
+            if (sizeof($result) === 0) {
+                $player->kick(TextFormat::RED. "Join again to the server!");
+            }
+            Session::$playerSettings[$pn] = $result[0];
+            $player->getLangSession()->apply();
+        });
+
+        $this->login[$pn] = 1;
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $ev): void
+    {
+        $ev->setJoinMessage(null);
+        $player = $ev->getPlayer();
+        $player->setImmobile();
+
+        if (!$player instanceof NetworkPlayer) return;
+        $pn = $player->getName();
+
+        if ($player->hasPermission("lobby.fly")) $player->setAllowFlight(true);
+        /*TODO: Funcion para mover el jugador al lobby*/
+
+        if (isset($this->login[$pn])) {
+            unset($this->login[$pn]);
+            $this->join[$pn] = 1;
+        }
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $ev): void
+    {
+        $ev->setQuitMessage(null);
+    }
+
+    public function onExhaust(PlayerExhaustEvent $ev): void
+    {
+        $ev->setCancelled();
+    }
+
+    /**
+     * @todo check if the player falls into the void.
+     * @param PlayerMoveEvent $ev
+     */
+    public function onPlayerMove(PlayerMoveEvent $ev): void
+    {
+        $player = $ev->getPlayer();
+
+        if (!$player instanceof NetworkPlayer) return;
+        $pn = $player->getName();
+
+        if (isset($this->login[$pn]) || isset($this->move[$pn])) $ev->setCancelled();
+
+        if (isset($this->join[$pn])) {
+            unset($this->join[$pn]);
+            $this->move[$pn] = 1;
+            return;
+        }
+
+        if (isset($this->move[$pn])) {
+            $player->setImmobile(false);
+            unset($this->move[$pn]);
+        }
+
+        if (Spawn_Data['is.enabled']) {
+            if ($player->getY() <= Spawn_Data['world.void.minimum']) {
+                $player->teleport(new Position(Spawn_Data['pos.x'], Spawn_Data['pos.y'], Spawn_Data['pos.z'], $this->getNetwork()->getServerPM()->getLevelByName(Spawn_Data['world.name'])), Spawn_Data['player.yaw'], Spawn_Data['player.pitch']);
+            }
+        }
+    }
+
+    public function preventSlotChange(InventoryTransactionEvent $ev): void
+    {
+        $player = $ev->getTransaction()->getSource();
+
+        if (Spawn_Data['is.enabled']) {
+            $level = Spawn_Data['world.name'];
+        } else {
+            $level = $this->getNetwork()->getServerPM()->getDefaultLevel()->getName();
+        }
+
+        if ($player->getLevel()->getName() === $level) {
+            if (!$player->isOp()) $ev->setCancelled();
+        }
+    }
+
+    public function preventPlayerDamage(EntityDamageEvent $ev): void
+    {
+        $ev->setCancelled();
+    }
+
+    public function preventBreak(BlockBreakEvent $ev): void
+    {
+        $player = $ev->getPlayer();
+
+        if (Spawn_Data['is.enabled']) {
+            $level = Spawn_Data['world.name'];
+        } else {
+            $level = $this->getNetwork()->getServerPM()->getDefaultLevel()->getName();
+        }
+
+        if ($player->getLevel()->getName() === $level) {
+            if (!$player->isOp()) $ev->setCancelled();
+        }
+    }
+
+    public function preventPlace(BlockPlaceEvent $ev): void
+    {
+        $player = $ev->getPlayer();
+
+        if (Spawn_Data['is.enabled']) {
+            $level = Spawn_Data['world.name'];
+        } else {
+            $level = $this->getNetwork()->getServerPM()->getDefaultLevel()->getName();
+        }
+
+        if ($player->getLevel()->getName() === $level) {
+            if (!$player->isOp()) $ev->setCancelled();
         }
     }
 }
